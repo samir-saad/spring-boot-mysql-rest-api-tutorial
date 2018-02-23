2a56536bc3ad32a6aef9675674f9e727a86452f9
Samir Saad <samir.saadallah@gmail.com>
2018-02-22 20:28:14 -0500

    add paging

--- src/main/java/com/example/easynotes/controller/NoteController.java
+++ src/main/java/com/example/easynotes/controller/NoteController.java
@@ -1,13 +1,22 @@
 package com.example.easynotes.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.easynotes.model.Note;
 import com.example.easynotes.repository.NoteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
 
 /**
  * Created by rajeevkumarsingh on 27/06/17.
@@ -20,8 +29,9 @@ public class NoteController {
     NoteRepository noteRepository;
 
     @GetMapping("/notes")
-    public List<Note> getAllNotes() {
-        return noteRepository.findAll();
+    public Page<Note> getAllNotes(Pageable pageRequest) {
+    	return noteRepository.findAll(pageRequest);
+//        return noteRepository.findAll();
     }
 
     @GetMapping("/notes/{id}")
--- src/main/java/com/example/easynotes/repository/NoteRepository.java
+++ src/main/java/com/example/easynotes/repository/NoteRepository.java
@@ -1,11 +1,12 @@
 package com.example.easynotes.repository;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 import com.example.easynotes.model.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by rajeevkumarsingh on 27/06/17.
  */
-public interface NoteRepository extends JpaRepository<Note, Long> {
+public interface NoteRepository extends PagingAndSortingRepository<Note, Long> {
 
 }
